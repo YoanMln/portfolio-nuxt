9856c6c730e70e5cd5a568acc74613fc6c52b9a3
YoanMln <yoan.moulin@hotmail.fr>
2026-08-31 10:24:42 +0200

    chore: install and setup sass-embedded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '~/assets/scss/main.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/abstracts/variables" as *;',
+        },
+      },
+    },
+  },
 })
