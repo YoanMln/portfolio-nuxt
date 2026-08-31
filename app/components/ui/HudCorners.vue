f1532657f0fe98f30f3f281cfebc7f3f7ed19711
YoanMln <yoan.moulin@hotmail.fr>
2026-08-31 20:38:51 +0200

    fix: add aria-label for accessibility

--- app/components/ui/HudCorners.vue
+++ app/components/ui/HudCorners.vue
@@ -1,6 +1,6 @@
 <template>
-  <span class="corner corner--top-left" />
-  <span class="corner corner--top-right" />
-  <span class="corner corner--bottom-left" />
-  <span class="corner corner--bottom-right" />
+  <span class="corner corner--top-left" aria-hiden="true" />
+  <span class="corner corner--top-right" aria-hiden="true" />
+  <span class="corner corner--bottom-left" aria-hiden="true" />
+  <span class="corner corner--bottom-right" aria-hiden="true" />
 </template>
