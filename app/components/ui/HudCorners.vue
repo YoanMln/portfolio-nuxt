6b85932bbb4915ffecffa78412e5c987e0674f16
YoanMln <yoan.moulin@hotmail.fr>
2026-08-31 18:01:37 +0200

    feat: HudCorners component for cards style

---
+++ app/components/ui/HudCorners.vue
@@ -0,0 +1,6 @@
+<template>
+  <span class="corner corner--top-left" />
+  <span class="corner corner--top-right" />
+  <span class="corner corner--bottom-left" />
+  <span class="corner corner--bottom-right" />
+</template>
