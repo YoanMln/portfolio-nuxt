6ac6d374dae16715c842c3f8245a1087763a19f8
YoanMln <yoan.moulin@hotmail.fr>
2026-08-31 09:51:11 +0200

    feat: create Hero componenents

---
+++ app/components/Hero.vue
@@ -0,0 +1,13 @@
+<script setup>
+import information from '~/data/introduction/informations.json'
+</script>
+
+<template>
+  <section class="hero">
+    <h1 class="hero__name">{{ information.name }}</h1>
+    <p class="hero__status">
+      {{ information.job }}
+      <span>// statut : {{ information.statut }}</span>
+    </p>
+  </section>
+</template>
