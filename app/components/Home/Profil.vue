83d75bd6078906a6c84658e59b51bcdbc928db63
YoanMln <yoan.moulin@hotmail.fr>
2026-08-31 12:18:47 +0200

    feat: add Profil components

---
+++ app/components/Home/Profil.vue
@@ -0,0 +1,12 @@
+<script setup>
+import profil from '~/data/introduction/profil.json'
+</script>
+
+<template>
+  <section class="intro">
+    <h1 class="intro__title">Profil : {{ profil.profil }}</h1>
+    <div class="info__tags">
+      <span v-for="tag in profil.tags" :key="tag" class="tag">{{ tag }}</span>
+    </div>
+  </section>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <HomeHero />
+    <HomeProfil />
   </div>
 </template>
