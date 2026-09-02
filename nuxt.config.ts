b303055adb9635f493e419b1a287e7a81be87e24
YoanMln <yoan.moulin@hotmail.fr>
2026-09-02 14:24:31 +0200

    chore: add runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      email: '',
+      linkedin: '',
+      github: '',
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
