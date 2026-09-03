db650eeb450077265475613a666555ae998ae765
YoanMln <yoan.moulin@hotmail.fr>
2026-09-03 12:35:51 +0200

    Feat/scss foundations (#9)
    * chore: install nuxt fonts
    
    * feat: add scss foundations : colors, fonts, global style
    
    * fix: add after and before in reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,14 +9,21 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/fonts'],
   css: ['~/assets/css/main.css', '~/assets/scss/main.scss'],
 
+  fonts: {
+    families: [
+      { name: 'Chakra Petch', provider: 'google', weights: [500, 600, 700] },
+      { name: 'JetBrains Mono', provider: 'google', weights: [400, 700] },
+      { name: 'Inter', provider: 'google', weights: [400, 500] },
+    ],
+  },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "~/assets/scss/abstracts/variables" as *;',
+          additionalData: '@use "~/assets/scss/abstracts" as *;',
         },
       },
     },
