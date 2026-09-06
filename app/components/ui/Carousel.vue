38f5e153688bc2c75d21fb9e3ec618c1d650d78b
YoanMln <yoan.moulin@hotmail.fr>
2026-09-06 11:29:51 +0200

    feat: add dots on carousel

--- app/components/ui/Carousel.vue
+++ app/components/ui/Carousel.vue
@@ -9,6 +9,7 @@ defineProps({
     v-slot="{ item }"
     loop
     arrows
+    dots
     :autoplay="{ delay: 5000 }"
     wheel-gestures
     :items="slides"
@@ -19,6 +20,8 @@ defineProps({
       item: 'basis-full ps-0 h-full',
       prev: 'sm:start-8 carousel__arrow carousel__arrow--prev',
       next: 'sm:end-8 carousel__arrow carousel__arrow--next',
+      dots: 'absolute bottom-3 left-1/2 -translate-x-1/2 z-10',
+      dot: 'carousel__dot',
     }"
   >
     <img :src="item.src" :alt="item.alt" />
@@ -35,6 +38,17 @@ defineProps({
   &:hover {
     border-color: $core;
     box-shadow: 0 0 18px -4px rgba($core, 0.6);
+  }
+}
+:deep(.carousel__dot) {
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background-color: rgba($text, 0.25);
+
+  &[aria-selected='true'] {
+    background-color: $core;
+    box-shadow: 0 0 8px $core;
   }
 }
 </style>
