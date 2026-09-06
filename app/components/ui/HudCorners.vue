08a7afff01c2df5a116a51e732bf529008172da1
YoanMln <yoan.moulin@hotmail.fr>
2026-09-06 13:25:39 +0200

    feat: add HudCorners style (#16)
    * feat: add HudCorners style
    
    * fix: correct aria-hidden

--- app/components/ui/HudCorners.vue
+++ app/components/ui/HudCorners.vue
@@ -1,6 +1,45 @@
 <template>
-  <span class="corner corner--top-left" aria-hiden="true" />
-  <span class="corner corner--top-right" aria-hiden="true" />
-  <span class="corner corner--bottom-left" aria-hiden="true" />
-  <span class="corner corner--bottom-right" aria-hiden="true" />
+  <span class="corner corner--top-left" aria-hidden="true" />
+  <span class="corner corner--top-right" aria-hidden="true" />
+  <span class="corner corner--bottom-left" aria-hidden="true" />
+  <span class="corner corner--bottom-right" aria-hidden="true" />
 </template>
+
+<style scoped lang="scss">
+.corner {
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border-color: $gold;
+  border-style: solid;
+  border-width: 0;
+  pointer-events: none;
+}
+.corner--top-left {
+  top: -1px;
+  left: -1px;
+  border-top-width: 2px;
+  border-left-width: 2px;
+}
+
+.corner--top-right {
+  top: -1px;
+  right: -1px;
+  border-top-width: 2px;
+  border-right-width: 2px;
+}
+
+.corner--bottom-left {
+  bottom: -1px;
+  left: -1px;
+  border-bottom-width: 2px;
+  border-left-width: 2px;
+}
+
+.corner--bottom-right {
+  bottom: -1px;
+  right: -1px;
+  border-bottom-width: 2px;
+  border-right-width: 2px;
+}
+</style>
