f3fe77047ad454bef832a56d8422a0eaf192d28a
YoanMln <yoan.moulin@hotmail.fr>
2026-09-06 13:16:36 +0200

    feat: add HudCorners style

--- app/components/ui/HudCorners.vue
+++ app/components/ui/HudCorners.vue
@@ -4,3 +4,42 @@
   <span class="corner corner--bottom-left" aria-hiden="true" />
   <span class="corner corner--bottom-right" aria-hiden="true" />
 </template>
+
+<style scoped lang="scss">
+.corner {
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border-color: $gold;
+  border-style: solid;
+  border-width: 0;
+  pointer-events: none;
+}
+.corner--top-left {
+  top: -1px;
+  left: -1px;
+  border-top-width: 2px;
+  border-left-width: 2px;
+}
+
+.corner--top-right {
+  top: -1px;
+  right: -1px;
+  border-top-width: 2px;
+  border-right-width: 2px;
+}
+
+.corner--bottom-left {
+  bottom: -1px;
+  left: -1px;
+  border-bottom-width: 2px;
+  border-left-width: 2px;
+}
+
+.corner--bottom-right {
+  bottom: -1px;
+  right: -1px;
+  border-bottom-width: 2px;
+  border-right-width: 2px;
+}
+</style>
