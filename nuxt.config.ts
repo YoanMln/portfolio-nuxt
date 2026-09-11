7e08438b6f6fd05195d48e2587e84177b3a54b10
YoanMln <yoan.moulin@hotmail.fr>
2026-09-11 10:29:11 +0200

    feat: add space grotesk typo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
     families: [
       { name: 'Chakra Petch', provider: 'google', weights: [500, 600, 700] },
       { name: 'JetBrains Mono', provider: 'google', weights: [400, 700] },
-      { name: 'Inter', provider: 'google', weights: [400, 500] },
+      { name: 'Space Grotesk', provider: 'google', weights: [400, 500] },
     ],
   },
   vite: {
