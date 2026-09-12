2fe66c01c1f5b0c9a79013898032c2e06ab5c979
YoanMln <yoan.moulin@hotmail.fr>
2026-09-12 10:42:11 +0200

    feat: add lang fr and favicon link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'fr',
+      },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   runtimeConfig: {
     public: {
       email: '',
