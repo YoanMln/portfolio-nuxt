c35d9d52454d12e7d7546cfea2781b4ca925068d
YoanMln <yoan.moulin@hotmail.fr>
2026-09-12 11:47:45 +0200

    feat: add ogTitle, description, image

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,6 +8,12 @@ useSeoMeta({
   title: 'Yoan — Développeur web junior | Portfolio',
   description:
     'Développeur web junior issu de l’horlogerie, je combine précision, sens du détail et développement pour créer des expériences web soignées.',
+  ogTitle: 'Yoan — Développeur web junior | Portfolio',
+  ogDescription:
+    'Développeur web junior issu de l’horlogerie, je combine précision, sens du détail et développement pour créer des expériences web soignées.',
+  ogImage: 'https://yoanmln.netlify.app/images/og-cover.png',
+  ogType: 'website',
+  twitterCard: 'summary_large_image',
 })
 </script>
 
